37f4da11519f0d89517eb0b9dbb9d2e126c30bd8
ramesh <avighnadoni@gmail.com>
2026-04-22 16:54:33 +0530

    Added new model

---
+++ include/dbt/dbt_clinical_data/models/silver/dimensions/dim_patient.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='table') }}
+
+select
+    ID as patient_id,
+    BIRTHDATE as birth_date,
+    DEATHDATE as death_date,
+    FIRST as first_name,
+    LAST as last_name,
+    MARITAL as marital_status,
+    RACE as race,
+    ETHNICITY as ethnicity,
+    GENDER as gender,
+    BIRTHPLACE as birth_place,
+    ADDRESS as address,
+    CITY as city,
+    STATE as state,
+    COUNTY as county,
+    ZIP as zip,
+    LAT as latitude,
+    LON as longitude,
+    HEALTHCARE_EXPENSES as healthcare_expenses,
+    HEALTHCARE_COVERAGE as healthcare_coverage,
+    case when DEATHDATE is not null then 1 else 0 end as is_deceased,
+    datediff('year', BIRTHDATE, current_date) as age,
+    case
+        when datediff('year', BIRTHDATE, current_date) < 18 then '0-17'
+        when datediff('year', BIRTHDATE, current_date) between 18 and 35 then '18-35'
+        when datediff('year', BIRTHDATE, current_date) between 36 and 50 then '36-50'
+        when datediff('year', BIRTHDATE, current_date) between 51 and 65 then '51-65'
+        else '66+'
+    end as age_band,
+    LOAD_TS,
+    SOURCE_FILE,
+    BATCH_ID
+from {{ source('bronze', 'br_patients') }}
